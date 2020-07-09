94ce1510925734e7966346c22d7f50ead35e06db
Stephen Combs <stephen.jcombs@gmail.com>
2020-07-08 23:34:26 -0400

    Updates

---
+++ precache-manifest.7d5812839175c21c7abbccd9b732cb00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffef8d9958f5b60a793aee8398fb3587",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "769d9ca544af7cef523d",
+    "url": "/monsters-rolodex/static/css/main.a7bbcfa5.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769d9ca544af7cef523d",
+    "url": "/monsters-rolodex/static/js/main.d711dbcc.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
